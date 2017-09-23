7c302eeeb429255bb6d1834ec23ca350522675d0
Lillian Lee <lillianlee118@gmail.com>
2017-09-23 10:11:46 -0700

    progress

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var {User, userSchema} = require('./db/mgdb');
 
 var User = db.Model.extend({
   tableName: 'users',
